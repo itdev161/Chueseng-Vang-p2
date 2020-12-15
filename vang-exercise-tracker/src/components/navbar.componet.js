fa7f1d8bd09bab3d318a2036af2bf40d3d42fcc5
Chueseng Vang <chewyvang@Chuesengs-MacBook-Pro.local>
2020-12-15 14:38:10 -0600

    Updated navbar

--- vang-exercise-tracker/src/components/navbar.componet.js
+++ vang-exercise-tracker/src/components/navbar.componet.js
@@ -6,7 +6,7 @@ export default class navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-                <Link to="/" className="navbar-brand">ExerTracker</Link>
+                <Link to="/" className="navbar-brand">ExerciseTracker</Link>
                 <div className="collpase navbar-collapse">
                 <ul className="navbar-nav mr-auto">
                     <li className="navbar-item">
